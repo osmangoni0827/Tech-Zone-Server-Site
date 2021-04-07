261dec82151b2456ef4dae0c5d93fdfaa5cb58fb
Mohammad Osman Goni <osmangoni0827@gmail.com>
2021-04-07 23:07:48 +0600

    added

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ app.use(express.json());
 const port = 4000
 require('dotenv').config()
 
-
+//Mongodatabase 
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.77ufn.mongodb.net/EmaJohanDataBase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
+
 client.connect(err => {
   const productcollection = client.db("EmaJohanDataBase").collection("Products");
   const Ordercollection=client.db("EmaJohanDataBase").collection("Orders");
@@ -50,7 +54,7 @@ client.connect(err => {
       const Products=req.body;
       Ordercollection.insertOne(Products)
       .then(result=>{
-         res.send(result.insertedCount);
+         res.send(result);
       })
   })
   console.log('Database Connected');
